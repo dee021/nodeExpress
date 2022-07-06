2a1cc1fe216407d4b9e01078e1e15d23d0778a8d
dee021 <dee21@gachon.ac.kr>
2022-07-06 16:52:22 +0900

    .

--- projects/meadowlark/meadowlark.js
+++ projects/meadowlark/meadowlark.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const exHandlebars = require('express-handlebars')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.engine('handlebars', exHandlebars({
+    defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars')
+
+/*
 app.get('/', (req, res) => { // app.get : 라우트(route) 추가
     res.type('text/plain')
     res.send('Meadowlark Travel')
@@ -11,21 +18,29 @@ app.get('/about', (req, res) => {
     res.type('text/plain')
     res.send('About Meadowlark Travel')
 })
+*/
+
+// --> 뷰 사용
+app.get('/', (req, res) => res.render('home'))
+
+app.get('/about', (req, res) => res.render('about'))
 
 // get -> use
 // custom 404 page
 app.use((req, res) => { 
-    res.type('text/plain')
+    // res.type('text/plain')
     res.status(404)
-    res.send('404 - Not Found')
+    // res.send('404 - Not Found')
+    res.render('404')
 })
 
 // custom 500 page
 app.use((err, req, res, next) => {
     console.error(err.message)
-    res.type('text/plain')
+    // res.type('text/plain')
     res.status(500)
-    res.send('500 - Server Error')
+    // res.send('500 - Server Error')
+    res.render('500')
 })
 
 app.listen(port, () => console.log(
